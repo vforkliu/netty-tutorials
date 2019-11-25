cf2731a7855817c98add50841ac6e4927d34d9f6
liujun <liujun@forkliu.com>
2019-11-25 10:21:10 +0800

    Create project ChatServer & ChatClient

--- Cat/src/main/java/com/forkliu/netty/CatHttpServerHandler.java
+++ Cat/src/main/java/com/forkliu/netty/CatHttpServerHandler.java
@@ -8,6 +8,7 @@ import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
 import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 //浏览器的特性会发送一个/favicon.ico请求，获取网站的图标
 public class CatHttpServerHandler extends SimpleChannelInboundHandler<HttpObject>{
@@ -15,6 +16,12 @@ public class CatHttpServerHandler extends SimpleChannelInboundHandler<HttpObject
     //channelRead0是读取客户端的请求并且向客户端返回响应的一个方法
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        //io.netty.handler.codec.http.DefaultHttpRequest,io.netty.handler.codec.http.LastHttpContent$1
+        System.out.println(msg.getClass());
+
+        //打印出远程的地址，/0:0:0:0:0:0:0:1: 49734，本地线程的49734端口的线程和netty进行通信
+        System.out.println(ctx.channel().remoteAddress());
+        TimeUnit.SECONDS.sleep(8);
 
         if(msg instanceof HttpRequest){
             HttpRequest httpRequest = (HttpRequest)msg;
@@ -26,6 +33,7 @@ public class CatHttpServerHandler extends SimpleChannelInboundHandler<HttpObject
                 System.out.println("请求favicon.ico");
                 return;
             }
+
             //向客户端返回的内容
             ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
@@ -39,5 +47,41 @@ public class CatHttpServerHandler extends SimpleChannelInboundHandler<HttpObject
             //其实更合理的close连接应该判断是http1.O还是1.1来进行判断请求超时时间来断开channel连接。
             ctx.channel().close();
         }
+    }
+
+
+    //管道活跃
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active");
+        super.channelActive(ctx);
+    }
+
+    //管道注册
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered");
+        super.channelRegistered(ctx);
+    }
+
+    //通道被添加
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler added");
+        super.handlerAdded(ctx);
+    }
+
+    //管道不活跃了
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel inactive");
+        super.channelInactive(ctx);
+    }
+
+    //取消注册
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel unregistered");
+        super.channelUnregistered(ctx);
     }
 }
---
+++ ChatServer/src/main/java/com/forkliu/chat/ChatServer.java
@@ -0,0 +1,26 @@
+package com.forkliu.chat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class ChatServer {
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup wokerGroup = new NioEventLoopGroup();
+
+        try{
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,wokerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChatServerInializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            wokerGroup.shutdownGracefully();
+        }
+    }
+}
